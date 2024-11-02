98ec7724b31026388a5a9865a21f36abdde0cbd3
Emmet Larson <elars1002@gmail.com>
2024-11-02 01:10:40 -0400

    get/post/commands in python server
    - File and Command input/output

---
+++ WebServer/app.py
@@ -0,0 +1,38 @@
+from fileinput import filename
+
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+import os
+
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    text = request.form['text_field']
+    return f"Received text: {text}"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file was part of this request"
+    file = request.files['file']
+    if file.filename == '':
+        return "Error parsing filename"
+    if file:
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return redirect(url_for('uploaded_file', filename=file.filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
